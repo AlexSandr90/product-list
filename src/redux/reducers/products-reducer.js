87dffdf1dcf432b7a378fed5963d00f3b9302d22
Alex Gaytsuk <gayrsuk@gmail.com>
2021-09-22 07:17:32 +0300

    initial commit

---
+++ src/redux/reducers/products-reducer.js
@@ -0,0 +1,13 @@
+
+
+const initialState = {
+    products: []
+};
+
+export const productsReducer = (state = initialState, action) => {
+    switch (action.type) {
+
+        default:
+            return state;
+    }
+}
